f7fc3b69e74007504a472363ded638d098bf01be
TRichardVD <150446958+TRichardVD@users.noreply.github.com>
2025-01-13 14:06:46 +0100

    chore(https): enable https

--- app/server.js
+++ app/server.js
@@ -1,13 +1,18 @@
 const express = require("express");
-
+const https = require('https')
+const fs = require('fs');
 
 const app = express();
+
+const credentials = {
+    key: fs.readFileSync('./certificats/server.key'),
+    cert: fs.readFileSync('./certificats/server.crt')
+  };
+
 const userRoute = require('./routes/User');
 app.use('/user', userRoute);
 
-
-
 // Démarrage du serveur
-app.listen(443, () => {
+https.createServer(credentials, app).listen(443, () => {
     console.log('Server running on port 443');
 });
