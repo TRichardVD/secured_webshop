a58a8913b1ad45d2039b0456fadb110018f14fcd
TRichardVD <150446958+TRichardVD@users.noreply.github.com>
2025-03-10 11:10:50 +0100

    refactor(UserController): improve code readability and structure in user creation and data retrieval functions

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -1,8 +1,8 @@
 // Importation des modules nécessaires
-const jwt = require("../helper/jwt");
-const db = require("../model/database");
-const dotenv = require("dotenv");
-const hashage = require("../helper/hashage");
+const jwt = require('../helper/jwt');
+const db = require('../model/database');
+const dotenv = require('dotenv');
+const hashage = require('../helper/hashage');
 
 dotenv.config();
 
@@ -17,43 +17,43 @@ dotenv.config();
  * @returns {Promise<number | undefined>} - ID de l'utilisateur créé ou `undefined` en cas d'échec.
  */
 const createUser = async function ({ username, password }) {
-  if (!username || !password) {
-    console.error("Paramètres manquants pour la création d'utilisateur");
-    return undefined;
-  }
+    if (!username || !password) {
+        console.error("Paramètres manquants pour la création d'utilisateur");
+        return undefined;
+    }
 
-  // Hachage du mot de passe avec le sel
-  const pswHashed = hashage.calculateHash(10, password);
+    // Hachage du mot de passe avec le sel
+    const pswHashed = hashage.calculateHash(10, password);
 
-  // Insertion des informations de l'utilisateur dans la base de données
-  try {
-    const result = await db.pool.query(
-      `INSERT INTO ${db.tableUser} (username, passwordHashed) VALUES (?, ?)`,
-      [username, pswHashed]
-    );
+    // Insertion des informations de l'utilisateur dans la base de données
+    try {
+        const result = await db.pool.query(
+            `INSERT INTO ${db.tableUser} (username, passwordHashed) VALUES (?, ?)`,
+            [username, pswHashed]
+        );
 
-    console.log("User created : ", result);
-  } catch (err) {
-    console.log("Error creating user: ", err);
-    return undefined;
-  }
+        console.log('User created : ', result);
+    } catch (err) {
+        console.log('Error creating user: ', err);
+        return undefined;
+    }
 
-  // Récupération des données de l'utilisateur tout juste créé
+    // Récupération des données de l'utilisateur tout juste créé
 
-  try {
-    const data = await getData({ username, passwordHashed: pswHashed });
-  } catch (err) {
-    console.error(
-      "Erreur lors de la récupération des données utilisateur : ",
-      err
-    );
-    return undefined;
-  }
-  if (!data) {
-    console.error("Erreur : Utilisateur créé introuvable");
-    return undefined;
-  }
-  return data.id;
+    try {
+        const data = await getData({ username, passwordHashed: pswHashed });
+        if (!data) {
+            console.error('Erreur : Utilisateur créé introuvable');
+            return undefined;
+        }
+        return data.id;
+    } catch (err) {
+        console.error(
+            'Erreur lors de la récupération des données utilisateur : ',
+            err
+        );
+        return undefined;
+    }
 };
 
 /**
@@ -68,70 +68,70 @@ const createUser = async function ({ username, password }) {
  * @returns {Promise<Object | undefined>} - Objet contenant les données utilisateur ou `undefined` si pas trouvé.
  */
 const getData = async function (
-  conditions,
-  likeConditions = null,
-  token = undefined,
-  secure = true
+    conditions,
+    likeConditions = null,
+    token = undefined,
+    secure = true
 ) {
-  // Préparation des clauses WHERE et LIKE pour la requête SQL
-  const whereClause = [];
-  const values = [];
+    // Préparation des clauses WHERE et LIKE pour la requête SQL
+    const whereClause = [];
+    const values = [];
 
-  // Parcours des conditions pour construire la requête
-  if (conditions) {
-    for (const [key, value] of Object.entries(conditions)) {
-      whereClause.push(`${key} = ?`);
-      values.push(value);
+    // Parcours des conditions pour construire la requête
+    if (conditions) {
+        for (const [key, value] of Object.entries(conditions)) {
+            whereClause.push(`${key} = ?`);
+            values.push(value);
+        }
     }
-  }
-  if (likeConditions) {
-    for (const [key, value] of Object.entries(likeConditions)) {
-      whereClause.push(`${key} LIKE ?`);
-      values.push(value);
+    if (likeConditions) {
+        for (const [key, value] of Object.entries(likeConditions)) {
+            whereClause.push(`${key} LIKE ?`);
+            values.push(value);
+        }
     }
-  }
 
-  // Construction de la requête SQL complète
-  const query = `SELECT * FROM ${db.tableUser} ${
-    whereClause.length > 0 ? "WHERE " + whereClause.join(" AND ") : ""
-  } LIMIT 10`;
-  console.log(query, values);
+    // Construction de la requête SQL complète
+    const query = `SELECT * FROM ${db.tableUser} ${
+        whereClause.length > 0 ? 'WHERE ' + whereClause.join(' AND ') : ''
+    } LIMIT 10`;
+    console.log(query, values);
 
-  // Exécution de la requête SQL
-  const [result] = await db.pool.query(query, values);
+    // Exécution de la requête SQL
+    const [result] = await db.pool.query(query, values);
 
-  // Traitement du résultat (filtrage sécurisé si nécessaire)
-  if (result.length === 0) {
-    return undefined;
-  }
-
-  if (!secure) {
-    // Retourne tous les champs sans filtrage
-    return result[0];
-  }
-
-  // Filtrage des informations sensibles
-  const resultFiltered = result.map((element) => {
-    const { passwordHashed, ...rest } = element;
-    return rest;
-  });
-
-  try {
-    if (!token) {
-      // Retourne les données sans vérification de token
-      return resultFiltered[0];
+    // Traitement du résultat (filtrage sécurisé si nécessaire)
+    if (result.length === 0) {
+        return undefined;
     }
-    // Vérification du token et des permissions administrateur
-    const tokenData = await jwt.verifyToken(token);
-    const UserData = await getData({ id: tokenData.sub }, null, null, true);
-    if (UserData.isAdmin === 1) {
-      // Si administrateur, retourne toutes les données sans filtrage
-      return result;
+
+    if (!secure) {
+        // Retourne tous les champs sans filtrage
+        return result[0];
     }
-    return resultFiltered[0];
-  } catch (err) {
-    return resultFiltered[0];
-  }
+
+    // Filtrage des informations sensibles
+    const resultFiltered = result.map((element) => {
+        const { passwordHashed, ...rest } = element;
+        return rest;
+    });
+
+    try {
+        if (!token) {
+            // Retourne les données sans vérification de token
+            return resultFiltered[0];
+        }
+        // Vérification du token et des permissions administrateur
+        const tokenData = await jwt.verifyToken(token);
+        const UserData = await getData({ id: tokenData.sub }, null, null, true);
+        if (UserData.isAdmin === 1) {
+            // Si administrateur, retourne toutes les données sans filtrage
+            return result;
+        }
+        return resultFiltered[0];
+    } catch (err) {
+        return resultFiltered[0];
+    }
 };
 
 module.exports = { createUser, getData };
